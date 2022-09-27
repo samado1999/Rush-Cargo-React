3c59a3fbbf2906cc0a224999dcad8ffca0e94a9b
samado1999 <36894271+samado1999@users.noreply.github.com>
2022-09-26 19:23:29 -0500

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { Helmet } from 'react-helmet'
 
-import NavigationLinks from '../components/navigation-links'
 import CallToActionWhatsApp from '../components/call-to-action-whats-app'
 import SolutionCard from '../components/solution-card'
 import TextCard from '../components/text-card'
@@ -31,10 +30,9 @@ const Home = (props) => {
         <header data-role="Header" className="home-header">
           <img
             alt="image"
-            src="a960be97-392f-46eb-bb35-0cec84c98969"
+            src="/playground_assets/rush-icon-2-200h.png"
             className="home-image1"
           />
-          <NavigationLinks rootClassName="navigation-links-root-class-name8"></NavigationLinks>
           <div className="home-icon-group">
             <svg viewBox="0 0 877.7142857142857 1024" className="home-icon">
               <path d="M562.857 556.571c9.714 0 102.857 48.571 106.857 55.429 1.143 2.857 1.143 6.286 1.143 8.571 0 14.286-4.571 30.286-9.714 43.429-13.143 32-66.286 52.571-98.857 52.571-27.429 0-84-24-108.571-35.429-81.714-37.143-132.571-100.571-181.714-173.143-21.714-32-41.143-71.429-40.571-110.857v-4.571c1.143-37.714 14.857-64.571 42.286-90.286 8.571-8 17.714-12.571 29.714-12.571 6.857 0 13.714 1.714 21.143 1.714 15.429 0 18.286 4.571 24 19.429 4 9.714 33.143 87.429 33.143 93.143 0 21.714-39.429 46.286-39.429 59.429 0 2.857 1.143 5.714 2.857 8.571 12.571 26.857 36.571 57.714 58.286 78.286 26.286 25.143 54.286 41.714 86.286 57.714 4 2.286 8 4 12.571 4 17.143 0 45.714-55.429 60.571-55.429zM446.857 859.429c197.714 0 358.857-161.143 358.857-358.857s-161.143-358.857-358.857-358.857-358.857 161.143-358.857 358.857c0 75.429 24 149.143 68.571 210.286l-45.143 133.143 138.286-44c58.286 38.286 127.429 59.429 197.143 59.429zM446.857 69.714c237.714 0 430.857 193.143 430.857 430.857s-193.143 430.857-430.857 430.857c-72.571 0-144.571-18.286-208.571-53.714l-238.286 76.571 77.714-231.429c-40.571-66.857-61.714-144-61.714-222.286 0-237.714 193.143-430.857 430.857-430.857z"></path>
@@ -56,7 +54,7 @@ const Home = (props) => {
               <div className="home-container02">
                 <img
                   alt="image"
-                  src="a960be97-392f-46eb-bb35-0cec84c98969"
+                  src="/playground_assets/rush-icon-2-200h.png"
                   className="home-image2"
                 />
                 <div
@@ -71,21 +69,17 @@ const Home = (props) => {
               <div className="home-container03">
                 <div className="home-sidebar">
                   <nav className="home-nav1">
-                    <a href="#home" className="home-link">
-                      Inicio
-                    </a>
-                    <a href="#about" className="home-link1">
+                    <span className="home-text">Inicio</span>
+                    <span className="home-text01">
                       <span>Sobre nosotros</span>
                       <br></br>
-                    </a>
-                    <a href="#services" className="home-link2">
+                    </span>
+                    <span className="home-text04">
                       <span>Servicios</span>
                       <br></br>
-                    </a>
-                    <a href="#contact" className="home-link3">
-                      Contactos
-                    </a>
-                    <span className="home-text04">Blog</span>
+                    </span>
+                    <span className="home-text07">Contactos</span>
+                    <span className="home-text08">Blog</span>
                   </nav>
                   <div className="home-icon-group1">
                     <svg
@@ -124,29 +118,34 @@ const Home = (props) => {
           </div>
         </header>
         <div className="home-container04">
+          <CallToActionWhatsApp rootClassName="call-to-action-whats-app-root-class-name2"></CallToActionWhatsApp>
+        </div>
+      </div>
+      <div className="home-container05">
+        <div className="home-container06">
           <CallToActionWhatsApp rootClassName="call-to-action-whats-app-root-class-name1"></CallToActionWhatsApp>
         </div>
       </div>
-      <div id="home" className="home-container05">
+      <div id="home" className="home-container07">
         <SolutionCard
           heading="Todas las soluciones en transporte de mercancía"
           rootClassName="solution-card-root-class-name"
         ></SolutionCard>
       </div>
-      <div className="home-container06">
+      <div className="home-container08">
         <TextCard
           text="Un servicio que te ayuda a traer todas tus compras del exterior"
           rootClassName="text-card-root-class-name"
         ></TextCard>
       </div>
-      <div className="home-container07">
+      <div className="home-container09">
         <button className="home-button button">¡Contactar!</button>
-        <span className="home-text05">
+        <span className="home-text09">
           <span>Solicita tu asesoría personalizada</span>
           <br></br>
         </span>
       </div>
-      <div className="home-container08">
+      <div className="home-container10">
         <HeadingCard
           heading="Experiencia"
           rootClassName="heading-card-root-class-name"
@@ -162,7 +161,7 @@ const Home = (props) => {
       </div>
       <SocialMediaCard rootClassName="social-media-card-root-class-name"></SocialMediaCard>
       <ServiceMethodCard rootClassName="service-method-card-root-class-name"></ServiceMethodCard>
-      <div id="about" className="home-container09">
+      <div id="about" className="home-container11">
         <AboutCard
           text="Es una empresa colombiana que ofrece los servicios de transporte de mercancía a nivel internacional."
           heading="Rush Cargo"
@@ -170,16 +169,16 @@ const Home = (props) => {
         ></AboutCard>
         <AboutCard rootClassName="about-card-root-class-name1"></AboutCard>
       </div>
-      <div className="home-container10">
-        <div className="home-container11">
-          <div className="home-container12">
+      <div className="home-container12">
+        <div className="home-container13">
+          <div className="home-container14">
             <svg viewBox="0 0 1024 1024" className="home-icon22">
               <path d="M682 554q56 0 122 16t122 52 56 82v106h-256v-106q0-88-84-148 14-2 40-2zM342 554q56 0 122 16t121 52 55 82v106h-598v-106q0-46 56-82t122-52 122-16zM342 470q-52 0-90-38t-38-90 38-90 90-38 89 38 37 90-37 90-89 38zM682 470q-52 0-90-38t-38-90 38-90 90-38 90 38 38 90-38 90-90 38z"></path>
             </svg>
           </div>
-          <div className="home-container13">
-            <h1 className="home-text08">Personalización</h1>
-            <span className="home-text09">
+          <div className="home-container15">
+            <h1 className="home-text12">Personalización</h1>
+            <span className="home-text13">
               <span>
                 Contamos con una amplia experiencia en el manejo de mercancías y
                 podemos ayudarte a traer eso que necesitas sin problemas ni
@@ -189,18 +188,18 @@ const Home = (props) => {
             </span>
           </div>
         </div>
-        <div className="home-container14">
-          <div className="home-container15">
+        <div className="home-container16">
+          <div className="home-container17">
             <svg viewBox="0 0 1060.5714285714284 1024" className="home-icon24">
               <path d="M365.714 804.571c0-40-33.143-73.143-73.143-73.143s-73.143 33.143-73.143 73.143 33.143 73.143 73.143 73.143 73.143-33.143 73.143-73.143zM146.286 512h219.429v-146.286h-90.286c-2.286 0-10.857 3.429-12.571 5.143l-111.429 111.429c-1.714 1.714-5.143 10.286-5.143 12.571v17.143zM877.714 804.571c0-40-33.143-73.143-73.143-73.143s-73.143 33.143-73.143 73.143 33.143 73.143 73.143 73.143 73.143-33.143 73.143-73.143zM1024 182.857v585.143c0 42.286-44.571 36.571-73.143 36.571 0 80.571-65.714 146.286-146.286 146.286s-146.286-65.714-146.286-146.286h-219.429c0 80.571-65.714 146.286-146.286 146.286s-146.286-65.714-146.286-146.286h-36.571c-28.571 0-73.143 5.714-73.143-36.571 0-20 16.571-36.571 36.571-36.571v-182.857c0-40.571-5.714-85.714 25.714-117.143l113.143-113.143c14.286-14.286 41.714-25.714 62.286-25.714h91.429v-109.714c0-20 16.571-36.571 36.571-36.571h585.143c20 0 36.571 16.571 36.571 36.571z"></path>
             </svg>
           </div>
-          <div className="home-container16">
-            <h1 className="home-text12">
+          <div className="home-container18">
+            <h1 className="home-text16">
               <span>Envío hasta tu puerta</span>
               <br></br>
             </h1>
-            <span className="home-text15">
+            <span className="home-text19">
               <span>
                 Te entregamos todos tus envíos hasta la puerta de tu casa u
                 oficina.
@@ -209,18 +208,18 @@ const Home = (props) => {
             </span>
           </div>
         </div>
-        <div className="home-container17">
-          <div className="home-container18">
+        <div className="home-container19">
+          <div className="home-container20">
             <svg viewBox="0 0 658.2857142857142 1024" className="home-icon26">
               <path d="M182.857 438.857h292.571v-109.714c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286v109.714zM658.286 493.714v329.143c0 30.286-24.571 54.857-54.857 54.857h-548.571c-30.286 0-54.857-24.571-54.857-54.857v-329.143c0-30.286 24.571-54.857 54.857-54.857h18.286v-109.714c0-140.571 115.429-256 256-256s256 115.429 256 256v109.714h18.286c30.286 0 54.857 24.571 54.857 54.857z"></path>
             </svg>
           </div>
-          <div className="home-container19">
-            <h1 className="home-text18">
+          <div className="home-container21">
+            <h1 className="home-text22">
               <span>Seguro de mercancía</span>
               <br></br>
             </h1>
-            <span className="home-text21">
+            <span className="home-text25">
               <span>
                 Manejamos diferentes tipos de seguros para que tus productos
                 siempre estén protegidos.
@@ -230,13 +229,13 @@ const Home = (props) => {
           </div>
         </div>
       </div>
-      <div id="services" className="home-container20">
-        <h1 className="home-text24">
+      <div id="services" className="home-container22">
+        <h1 className="home-text28">
           <span>Nuestros servicios</span>
           <br></br>
         </h1>
       </div>
-      <div className="home-container21">
+      <div className="home-container23">
         <ServiceCard
           heading="Courier"
           image_src="/playground_assets/airplane-200h.png"
@@ -251,28 +250,28 @@ const Home = (props) => {
           heading="Urbanos"
           rootClassName="service-card-root-class-name"
         ></ServiceCard>
-        <div className="home-container22">
-          <div className="home-container23">
+        <div className="home-container24">
+          <div className="home-container25">
             <img
               alt="image"
               src="/playground_assets/container-200h.png"
               className="home-image3"
             />
           </div>
-          <h1 className="home-text27">
+          <h1 className="home-text31">
             <span>Seguros Carga</span>
             <br></br>
           </h1>
         </div>
       </div>
       <SocialMediaCard rootClassName="social-media-card-root-class-name1"></SocialMediaCard>
-      <div className="home-container24">
-        <span className="home-text30">
+      <div className="home-container26">
+        <span className="home-text34">
           <span>Síguenos en redes sociales</span>
           <br></br>
         </span>
       </div>
-      <div id="contact" className="home-container25">
+      <div id="contact" className="home-container27">
         <FormCard rootClassName="form-card-root-class-name"></FormCard>
       </div>
       <Footer rootClassName="footer-root-class-name"></Footer>
