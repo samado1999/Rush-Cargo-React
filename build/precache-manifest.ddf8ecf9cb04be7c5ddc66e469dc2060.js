62540a5cb208fddd315eb3c872d01a4b4c5154e1
Samuel <samado9964@gmail.com>
2022-10-10 16:15:34 -0500

    new build

---
+++ build/precache-manifest.ddf8ecf9cb04be7c5ddc66e469dc2060.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f48184c3db750ba3c1d1",
+    "url": "/static/css/main.69dd9eaa.chunk.css"
+  },
+  {
+    "revision": "f48184c3db750ba3c1d1",
+    "url": "/static/js/main.ab52ff11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "032c86cd63737d0f3286",
+    "url": "/static/js/2.33a58cce.chunk.js"
+  },
+  {
+    "revision": "0362383f31525fce45ce73c6c2d45a0c",
+    "url": "/index.html"
+  }
+];
