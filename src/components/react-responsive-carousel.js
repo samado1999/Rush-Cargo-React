eb6c09201a02c2c2532a854c65aeebed087836ae
Samuel <samado9964@gmail.com>
2022-10-18 10:26:28 -0500

    new build

--- src/components/react-responsive-carousel.js
+++ src/components/react-responsive-carousel.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React from 'react';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import './react-responsive-carousel.css'
+import './react-responsive-carousel.css';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const ReactResponsiveCarousel = (props) => {
   return (
     <div
       className={`react-responsive-carousel-container ${props.rootClassName} `}
-    ></div>
-  )
-}
+    >
+      <div className='service-method-card-container1'>
+        <Carousel className='service-method-card-image'>
+          <div>
+            <img src={props.image1} />
+          </div>
+          <div>
+            <img src={props.image2} />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
 
 ReactResponsiveCarousel.defaultProps = {
   rootClassName: '',
-}
+  image1: '/playground_assets/Carousel-1.jpeg',
+  image2: '/playground_assets/Carousel-2.jpeg',
+};
 
 ReactResponsiveCarousel.propTypes = {
   rootClassName: PropTypes.string,
-}
+  image1: PropTypes.string,
+  image2: PropTypes.string,
+};
 
-export default ReactResponsiveCarousel
+export default ReactResponsiveCarousel;
