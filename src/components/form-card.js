1edcdf1e930f949b313790af7f56224e8d43b84d
Samuel <samado9964@gmail.com>
2022-10-25 20:19:46 -0500

    form functions

--- src/components/form-card.js
+++ src/components/form-card.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-import './form-card.css'
+import ReactWhatsapp from 'react-whatsapp';
+
+import './form-card.css';
 
 const FormCard = (props) => {
+  const [message, setMessage] = useState('');
+
+  function invalid(event) {
+    event.target.setCustomValidity('Ingrese su mensaje');
+  }
+
+  function setMessageValue(event) {
+    setMessage(event.target.value);
+  }
+
+  function handleFormSubmit(event) {}
   return (
     <div className={`form-card-container ${props.rootClassName} `}>
-      <form className="form-card-form">
-        <div className="form-card-container1">
-          <span className="form-card-text">{props.text2}</span>
+      <form className='form-card-form'>
+        <div className='form-card-container1'>
+          <span className='form-card-text'>{props.text2}</span>
           <textarea
             placeholder={props.textarea_placeholder}
-            id="message-area"
-            className="form-card-textarea textarea"
-            required
+            id='message-area'
+            className='form-card-textarea textarea'
+            value={message}
+            onChange={setMessageValue}
           ></textarea>
         </div>
-        <div className="form-card-container2">
-          <button className="form-card-button button">{props.button}</button>
+        <div className='form-card-container2'>
+          <ReactWhatsapp
+            className='form-card-button button'
+            number='+57 316 8331920'
+            message={message}
+            onClick={handleFormSubmit}
+          >
+            {props.button}
+          </ReactWhatsapp>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 FormCard.defaultProps = {
   text2: 'Envíanos un mensaje 😃',
@@ -31,7 +52,7 @@ FormCard.defaultProps = {
   rootClassName: '',
   button: '¡Contactar!',
   link_button: 'https://wa.me/message/F4ZSFNDG5G64D1',
-}
+};
 
 FormCard.propTypes = {
   text2: PropTypes.string,
@@ -39,6 +60,6 @@ FormCard.propTypes = {
   rootClassName: PropTypes.string,
   button: PropTypes.string,
   link_button: PropTypes.string,
-}
+};
 
-export default FormCard
+export default FormCard;
